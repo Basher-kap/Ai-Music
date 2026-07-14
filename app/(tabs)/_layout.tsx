b6cc9ce2b9d99dbed44a62e303c02af5a7572499
Basher-kap <kap.bash@gmail.com>
2026-07-14 10:50:52 +0800

    Test OTA: temp tab title change

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -26,7 +26,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Home!!',
           headerShown: false,
           tabBarIcon: ({ color, focused }) => (
             <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
